fb5e8bf5b6db1c3a106199c362263efbc2e8e00d
Justin Kemp <jkemp0790@gmail.com>
2021-09-04 12:12:02 -0500

    added MapBox api

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoianVzdGlua2VtcDEwIiwiYSI6ImNrdDYxOXJpYzBlM2Yyb211NnYwYjgwaHkifQ.DHD31_ysSqszwW7LNWQs-g";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
 
+function FindCity() {
+  var City = $("#cityName").val();
 
+  var Coord =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    City +
+    "&appid=b16f1aea6c8a8adc1ebc2fd17697b89a&units=imperial";
 
+  fetch(Coord)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      } else {
+        return response.json();
+      }
+    })
+    .then(function (locRes) {
+      //   console.log(locRes);
+      var lat = locRes.city.coord.lat;
+      var lon = locRes.city.coord.lon;
+      console.log(lat, lon);
+    });
+}
 
-
-
-
-
-
-$('searchButton').on('click', )
+$("#searchButton").on("click", FindCity);
