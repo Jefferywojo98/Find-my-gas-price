bf967f36abbaa36220a0b70a60ee33e7e587df5a
Jeffery <jefferywojo1998@gmail.com>
2021-09-07 20:07:53 -0500

    adding the value to the list

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 
+mapboxgl.accessToken =
+  "pk.eyJ1IjoianVzdGlua2VtcDEwIiwiYSI6ImNrdDYxOXJpYzBlM2Yyb211NnYwYjgwaHkifQ.DHD31_ysSqszwW7LNWQs-g";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+function FindStation(lat, lon){
+    var Coord = 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=HknVf14CYKrMly49l59op0xR6ZmLU87fawrITIkg&latitude=' + lat + '&longitude=' + lon + '&radius=500.0';
+
+    fetch(Coord)
+        .then(function(response){
+            if(!response.ok){
+                throw response.json()
+            }
+            else{
+                return response.json()
+            }
+        })
+        .then(function (locRes){
+            console.log(locRes)
+            
+            var Stations = locRes.fuel_stations
+
+            $('.list').append('<ul>').addClass('Stations')
+
+            // for(i = 0; i < Stations.length; i++){
+            //     // console.log(Stations[i])
+            //     $('<li>').addClass('Closer')
+            //     $('.Stations').append('.Closer').html(Stations[i].distance)
+            // }
+            
+        })
+}
+
 function FindCity(){
     var City = $('#cityName').val()
 
@@ -16,11 +53,10 @@ function FindCity(){
         .then(function (locRes){
             var lat = locRes.city.coord.lat
             var lon = locRes.city.coord.lon
-            console.log(lat);
-        })
+            
+            FindStation()
+        });
 }
 
 
-
-
-$('searchButton').on('click', FindCity)
+$('#searchButton').on('click', FindCity)
