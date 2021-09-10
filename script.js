e523f96490f2086667d0d6f8bd68f9af79e74e20
Dvera97 <42497806+PassionPhoenix@users.noreply.github.com>
2021-09-09 20:40:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,52 +27,60 @@ function FindStation(){
         })
         .then(function (locRes){
 
+            console.log(locRes)
+
             $('.Stations').children('li').remove();
             $('.list').children('ul').remove();
 
             var Stations = locRes.fuel_stations;
             var lon = 0;
             var lat = 0;
-
-
-            $('.list').append('<ul>').addClass('Stations');
-            
-            for(i = 0; i < locRes.fuel_stations.length; i++){
-                lon = lon + locRes.fuel_stations[i].longitude;
-                lat = lat + locRes.fuel_stations[i].latitude;
-            };
-
-            lon = lon / locRes.fuel_stations.length;
-            lat = lat / locRes.fuel_stations.length;
-
-            map.flyTo({center: [lon, lat],});
-
-            if (currentMarkers!==null) {
-                for (var i = currentMarkers.length - 1; i >= 0; i--) {
-                  currentMarkers[i].remove();
-                };
-            };
             
             $('.Loading').append('<h3>').addClass('Waiting').html('One Moment Please...');
+            
+            if (currentMarkers!==null) {
+                for (var i = currentMarkers.length - 1; i >= 0; i--) {
+                currentMarkers[i].remove();
+                };
+            };
 
-            for(i = 0; i < Stations.length; i++){
-                 $('<li>').appendTo('.Stations').addClass('Closer ' + locRes.fuel_stations[i].fuel_type_code).html('City: ' + locRes.fuel_stations[i].city + ' Station Name: ' + locRes.fuel_stations[i].station_name);
-                 // Create a default Marker and add it to the map.
-                 const marker = new mapboxgl.Marker()
-                .setLngLat([locRes.fuel_stations[i].longitude, locRes.fuel_stations[i].latitude])
-                .setPopup(
-                    new mapboxgl.Popup({ offset: 25 })
-                    .setHTML(`<h3>City: ${locRes.fuel_stations[i].city}</h3><h5>Station: ${locRes.fuel_stations[i].station_name}</h5><p>Address: ${locRes.fuel_stations[i].street_address}</p>`)
-                )
-                .addTo(map);
-                currentMarkers.push(marker);
+            if(locRes.fuel_stations.length > 0){
+                for(i = 0; i < locRes.fuel_stations.length; i++){
+                    lon = lon + locRes.fuel_stations[i].longitude;
+                    lat = lat + locRes.fuel_stations[i].latitude;
+                };
+
+                lon = lon / locRes.fuel_stations.length;
+                lat = lat / locRes.fuel_stations.length;
+
+                map.flyTo({center: [lon, lat],});
                 
-                if(i == (Stations.length - 1)){
-                    $('.Waiting').html('')
+                $('.list').append('<ul>').addClass('Stations');
+
+                for(i = 0; i < Stations.length; i++){
+                    $('<li>').appendTo('.Stations').addClass('Closer ' + locRes.fuel_stations[i].fuel_type_code).html('City: ' + locRes.fuel_stations[i].city + ' Station Name: ' + locRes.fuel_stations[i].station_name);
+                    // Create a default Marker and add it to the map.
+                    const marker = new mapboxgl.Marker()
+                    .setLngLat([locRes.fuel_stations[i].longitude, locRes.fuel_stations[i].latitude])
+                    .setPopup(
+                        new mapboxgl.Popup({ offset: 25 })
+                        .setHTML(`<h3>City: ${locRes.fuel_stations[i].city}</h3><h5>Station: ${locRes.fuel_stations[i].station_name}</h5><p>Address: ${locRes.fuel_stations[i].street_address}</p>`)
+                    )
+                    .addTo(map);
+                    currentMarkers.push(marker);
+                    
+                    if(i == (Stations.length - 1)){
+                        $('.Waiting').html('')
+                    }
                 }
+            }else{
+                $('.Waiting').html('Results Not Found')
             }
 
+            
+
         });
+
     };
             
 
